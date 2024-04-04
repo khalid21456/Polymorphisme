c0e673f479013f040981f9d52db0b0c909d6795c
khalid21456 <khalidedaoudi8@gmail.com>
2024-04-04 18:20:11 +0100

    init

---
+++ src/polymorphisme/Fruit.java
@@ -0,0 +1,15 @@
+package polymorphisme;
+
+public abstract class Fruit {
+	
+	protected int poids;
+	
+	public Fruit() {
+		System.out.println("Creation d'un fruit");
+	}
+	
+	public void afficher() {
+		System.out.println("C'est un fruit");
+	}
+	
+}
