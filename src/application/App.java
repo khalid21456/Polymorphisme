1ebc69d3d0c7b977967b8819b951454a31b87047
khalid21456 <khalidedaoudi8@gmail.com>
2024-04-04 21:07:01 +0100

    Polymorphisme

--- src/application/App.java
+++ src/application/App.java
@@ -6,11 +6,25 @@ public class App {
 
 	public static void main(String[] args) {
 			
-		Fruit f;
-		f = new Pomme(20);
+		Fruit[] fruits;
+		fruits = new Fruit[5];
 		
-		//f.afficherPoids()
-		f.afficher();
+		Pomme p = new Pomme(20);
+		Orange o = new Orange(50);
+		Fruit f1 = new Pomme(25);
+		Fruit f2 = new Orange(21);
+		
+		fruits[0] = p;
+		fruits[1] = o;
+		fruits[2] = f1;
+		fruits[3] = f2;
+		
+		for(int i=0;i<5;i++) {
+			if(fruits[i] instanceof Orange) {
+				((Orange)fruits[i]).afficherPoids();
+			}
+		}
+		
 	}
 
 }
